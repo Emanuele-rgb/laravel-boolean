4bf7b3beb3b1a8a966e0f411f4431d70892db104
Emanuele Sanquirico <manu.sanquirico@gmail.com>
2020-06-12 00:46:23 +0200

    Added students

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,9 +6,32 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
-    public function index(){
+    private $students;
 
+    public function __construct()
+    {
+        $this->students = [
+            [
+                'id' => 1,
+                'img' => 'https://www.boolean.careers/images/students/biagini.png',
+                'nome' => 'Alessandro Biagini',
+                'eta' => 25,
+                'genere' => 'm',
+                'azienda' => 'DISC SPA',
+                'ruolo' => 'web developer',
+                'descrizione' => 'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
+            ],
+        ];
+    }
+
+    
+    public function index(){
+        
+        
+            $students = $this->students;
+        
+
+        return view ('students.index', compact('students'));
     }
 
     public function show($id){
