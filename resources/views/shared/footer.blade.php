17884ee796b92b28abb46ba4d3c898049e3ff36f
Emanuele Sanquirico <manu.sanquirico@gmail.com>
2020-06-12 00:19:48 +0200

    Skeleton ready

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+    <nav class="nav-footer">
+        <div class="footer-brand">
+            Boolean
+            <ul>
+                <li><a href="{{ route('static-page.home') }}">Home</a></li>
+                <li><a href="{{ route('static-page.faq') }}">Domande frequenti</a></li>
+                <li><a href="{{ route('static-page.privacy') }}">Privacy</a></li>
+            </ul>
+        </div>
+    </nav>
+</footer>
+    
+</body>
+</html>
